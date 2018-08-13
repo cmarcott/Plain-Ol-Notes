ad078391141122ee13ea984c2b7c4b650ec70c8f
Christopher Marcotte <christopherlmarcotte@gmail.com>
2018-08-13 17:27:03 -0400

    Plain 'Ol Notes

---
+++ Plain Ol' Notes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Plain Ol' Notes
+//
+//  Created by Christopher Marcotte on 2018-05-27.
+//  Copyright © 2018 christophermarcotte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
